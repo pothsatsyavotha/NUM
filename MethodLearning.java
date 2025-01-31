08da9a8a6d069b904ce7a332ae5d753da80414d1
pothsatsyavotha <pothvotha@gmail.com>
2025-01-31 16:44:55 +0700

    num year2

---
+++ MethodLearning.java
@@ -0,0 +1,10 @@
+import com.sun.net.httpserver.Headers;
+
+public class MethodLearning{
+    public static void helloWorld(){
+        System.out.println("Hello World!!");
+    }
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}
